6465f2a7477ddda23572f0b3d3fd0a7b61617112
relaxedrt <rodrigotejeria01@gmail.com>
2024-02-27 10:17:25 +0100

    nnuevas funciones y main creado

--- pruebas.py
+++ pruebas.py
@@ -52,6 +52,10 @@ def register():
     if dniexists or pwdmissmatch:
         print("Ha habido un error registrando el usuario.")
 
+#Con esta función nos loguearemos con un usuario siempre y cuando exista
+#uno bajo ese dni, en dicho caso devolveremos el error por pantalla y 
+#volveremos a la instancia principal.
+        
 def login():
     #Accedemos a las variables globales
     global activeuser
@@ -83,6 +87,8 @@ def login():
     #Si recorremos toda la base de datos y no encontramos el dni devolvemos user para saber que no existe
     print("user doesn't exists")
 
+#Con esta función nos desloguearemos de nuestra sesión actual
+
 def logout():
     #Accedemos a las variables globales
     global activeuser
@@ -91,7 +97,10 @@ def logout():
     activeuser = ""
     admin = False
 
-    return  "logged out"
+    print("logged out")
+
+#Con esta función crearemos una cuenta a nombre del usuario activo
+#o bajo el del usuario deseado con el admin
 
 def createaccount():
     #Accedemos a las variables globales
@@ -119,7 +128,7 @@ def createaccount():
                         database.cell(row=acc, column=3).value = user
                         print(f"La nueva cuenta de {user} es: {database.cell(row=acc, column=1).value}")
                         databasexlsx.save(databasedir) 
-                        break
+                        return 0
     else:
         for acc in range(2, 1048576):
             if database.cell(row=acc, column=1).value == None:
@@ -197,7 +206,8 @@ def deposit():
             if database.cell(row=acc, column = 1) == account:
                 #Actualizamos la columna del dinero
                 database.cell(row=acc, column = 2).value += money
-                break
+                databasexlsx.save(databasedir) 
+                return 0
     else:
         #Recorremos la lista de cuentas
         for acc in range(2, 1048576):
@@ -214,10 +224,11 @@ def deposit():
             if (database.cell(row=acc, column = 1).value == account) and (database.cell(row=acc, column = 3) == activeuser):
                 #Actualizamos la columna del dinero
                 database.cell(row=acc, column = 2).value += money
-                break
+                databasexlsx.save(databasedir)
+                return 0
             elif (database.cell(row=acc, column = 1).value == account) and (database.cell(row=acc, column = 3) != activeuser):
                 print("Esa no es tu cuenta. Intenta nuevamente.")
-                break
+                return 0
     print("No existe ese usuario.")
 
 def withdrawal():
@@ -255,10 +266,11 @@ def withdrawal():
                 if database.cell(row=acc, column = 2).value >= money:
                     #Actualizamos la columna del dinero
                     database.cell(row=acc, column = 2).value -= money
-                    break
+                    databasexlsx.save(databasedir)
+                    return 0
                 else:
                     print("No hay suficiente dinero.")
-                    break
+                    return 0
     else:
         #Recorremos la lista de cuentas
         for acc in range(2, 1048576):
@@ -276,12 +288,68 @@ def withdrawal():
                 if database.cell(row=acc, column = 2).value >= money:
                     #Actualizamos la columna del dinero
                     database.cell(row=acc, column = 2).value -= money
-                    break
+                    databasexlsx.save(databasedir)
+                    return 0
                 else:
                     print("No hay suficiente dinero.")
-                    break
+                    return 0
             elif (database.cell(row=acc, column = 1).value == account) and (database.cell(row=acc, column = 3) != activeuser):
                 print("Esa no es tu cuenta. Intenta nuevamente.")
-                break
+                return 0
     print("No existe ese usuario.")
 
+
+#Ciclo de programa
+while True:
+    #Si no hay un usuario activo, se muestra el menú de login/register
+    if  activeuser == "":  
+        print("====================================================")
+        print("//                   ARTIBANK SL                  //")
+        print("//  1.Iniciar sesión                              //")
+        print("//  2.Registrar usuario                           //")
+        print("//  3.Cerrar aplicación                           //")
+        print("====================================================")
+
+        opt = input("¿Que opción deseas ejecutar?: ")
+
+        if  int(opt)==1:
+            login()
+        elif int(opt)==2:
+            register()
+        elif int(opt)==3:
+            exit()
+        else:
+            print("No es una opción válida.\n")
+    #Si hay un usuario activo, se muestra el menú de login/register
+    else:
+        print("====================================================")
+        print("//                   ARTIBANK SL                  //")
+        print(f"//                   {activeuser}                  //")
+        print("//                                                //")
+        print("//  1.Chequear cuentas        4.Depositar         //")
+        print("//  2.Crear cuentas           5.Retirar           //")
+        print("//  3.Saldo total             6.Transferencia     //")
+        print("====================================================")
+        print("//  98.Logout                 99.EXIT             //")
+        print("====================================================")
+
+        opt = input("¿Que opción deseas ejecutar?: ")
+
+        if  int(opt)==1:
+            checkaccounts()
+        elif int(opt)==2:
+            createaccount()
+        #elif int(opt)==3:
+            #total()
+        elif int(opt)==4:
+            deposit()
+        elif int(opt)==5:
+            withdrawal()
+        #elif int(opt)==6:
+            #transfer()
+        elif int(opt)==98:
+            logout()
+        elif int(opt)==99:
+            exit()
+        else:
+            print("No es una opción válida.\n")
