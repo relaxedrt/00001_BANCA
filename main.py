7dcc325570f8ed6ba654f072c8dd05ffb4066063
relaxedrt <rodrigotejeria01@gmail.com>
2024-02-27 12:47:25 +0100

    rev 1.0 Finalización temporal del programa Aun se pueden añadir más cosas :)

--- main.py
+++ main.py
@@ -145,6 +145,7 @@ def createaccount():
                 return 0
     print("No existe ese usuario.")
 
+#Con esta función chequearemos el balance de una cuenta en concreto
 def checkaccounts():
     #Accedemos a las variables globales
     global activeuser
@@ -176,6 +177,7 @@ def checkaccounts():
             if database.cell(row=acc, column = 3).value == activeuser:
                 print(f"La cuenta {database.cell(row=acc, column = 1).value} del usuario {activeuser} tiene {database.cell(row=acc, column = 2).value}€")
 
+#Con esta función simularemos el deposito de dinero en un ATM
 def deposit():
     #Accedemos a las variables globales
     global activeuser
@@ -237,6 +239,7 @@ def deposit():
                 return 0
     print("No existe ese usuario.")
 
+#Con esta función simulamos la retirada de dinero en un ATM
 def withdrawal():
     #Accedemos a las variables globales
     global activeuser
@@ -306,6 +309,200 @@ def withdrawal():
                 return 0
     print("No existe ese usuario.")
 
+#Con esta función comprobaremos el total de dinero del usuario
+def total():
+    #Accedemos a las variables globales
+    global activeuser
+    global admin
+    money = 0
+
+    #Abrimos la base de datos
+    databasexlsx = openpyxl.load_workbook(databasedir)
+    database = databasexlsx["accounts"]
+    users = databasexlsx["login"]
+
+    print("====================================================")
+    print("//                   ARTIBANK SL                  //")
+    print("====================================================")
+    
+    if admin:
+        user = input("¿De que usuario quiere conocer el balance?: ")
+        for usr in range(2, 1048576):
+            #Comprobamos que exista ese usuario
+            if users.cell(row=usr, column=1).value == user:
+                #Recorremos la lista de cuentas
+                for acc in range(2, 1048576):
+                    #Si el usuario coincide con el dueño de la cuenta mostramos los datos por pantalla
+                    if database.cell(row=acc, column = 3).value == user:
+                        money += database.cell(row=acc, column = 2).value
+                print(f"El usuario {user} tiene {money}€.")
+    else:
+        #Recorremos la lista de cuentas
+        for acc in range(2, 1048576):
+            #Si el usuario coincide con el dueño de la cuenta mostramos los datos por pantalla
+            if database.cell(row=acc, column = 3).value == activeuser:
+                money += database.cell(row=acc, column = 2).value
+        print(f"Tus cuentas tienen un acumulado de {money}€.")
+    return 0
+
+#Con esta función realizaremos transferencias entre cuentas
+def transfer():
+    #Accedemos a las variables globales
+    global activeuser
+    global admin
+    money = 0
+
+    #Abrimos la base de datos
+    databasexlsx = openpyxl.load_workbook(databasedir)
+    database = databasexlsx["accounts"]
+    users = databasexlsx["login"]
+
+    print("====================================================")
+    print("//                   ARTIBANK SL                  //")
+    print("====================================================")
+    
+    if admin:
+        origin = input("Introduce la cuenta de origen de la transferencia: ")
+        destiny = input("Introduce la cuenta de destino de la transferencia: ")
+        money = input("Introduce la cantidad a transferir: ")
+
+        #Recorremos la lista de cuentas
+        for des in range(2, 1048576):
+            #Si la cuenta existe
+            if database.cell(row=des, column = 1).value == destiny:
+                destexist = True
+                break
+            else:
+                destexist = False
+        if  destexist:
+            #Comprobamos que exista la cuenta de origen y que tenga dinero
+            #Recorremos la lista de cuentas
+            for ori in range(2, 1048576):
+                #Si la cuenta existe
+                if database.cell(row=ori, column = 1).value == origin:
+                    oriexist = True
+                    if database.cell(row=ori, column = 2).value >= money:
+                        database.cell(row=ori, column = 2).value -= money
+                        database.cell(row=des, column = 2).value += money
+                        databasexlsx.save(databasedir)
+                        print("success")
+                        return 0
+                    else:
+                        print("Dinero isuficiente.")
+                        return 0
+                else:
+                    oriexist = False
+    else:
+        origin = input("Introduce la cuenta de origen de la transferencia: ")
+        destiny = input("Introduce la cuenta de destino de la transferencia: ")
+        money = int(input("Introduce la cantidad a transferir: "))
+
+        #Recorremos la lista de cuentas
+        for des in range(2, 1048576):
+            #Si la cuenta existe
+            if database.cell(row=des, column = 1).value == destiny:
+                destexist = True
+                break
+            else:
+                destexist = False
+        if  destexist:
+            #Comprobamos que exista la cuenta de origen y que tenga dinero
+            #Recorremos la lista de cuentas
+            for ori in range(2, 1048576):
+                #Si la cuenta existe
+                if database.cell(row=ori, column = 1).value == origin:
+                    oriexist = True
+                    if database.cell(row=ori, column = 3).value == activeuser:
+                        if database.cell(row=ori, column = 2).value >= money:
+                            database.cell(row=ori, column = 2).value -= money
+                            database.cell(row=des, column = 2).value += money
+                            databasexlsx.save(databasedir)
+                            print("success")
+                            return 0
+                        else:
+                            print("Dinero isuficiente.")
+                            return 0
+                    else:
+                        print("No puedes enviar dinero desde una cuenta que no es tuya.")
+                        return 0
+                else:
+                    oriexist = False
+    if not destexist:
+        print ("Cuenta de destino no encontrada.")
+        return 0
+    if not oriexist:
+        print ("Cuenta de origen no encontrada.")
+        return 0
+    
+#Funnción que permite borrar cuentas al admin de cualquiera y al usuario particular.
+def delaccount():
+    #Accedemos a las variables globales
+    global activeuser
+    global admin
+    money = 0
+
+    #Abrimos la base de datos
+    databasexlsx = openpyxl.load_workbook(databasedir)
+    database = databasexlsx["accounts"]
+    users = databasexlsx["login"]
+
+    print("====================================================")
+    print("//                   ARTIBANK SL                  //")
+    print("====================================================")
+    
+    if admin:
+        account = input("Que cuenta quieres borrar?: ")
+        #Recorremos la lista de cuentas
+        for acc in range(2, 1048576):
+            #Si la cuenta existe
+            if database.cell(row=acc, column = 1).value == account:
+                #Preguntamos si quiere confirmar el borrado
+                answer = str(input(f"¿Estás seguro/a de eliminar la cuenta {account}? s/n: ")).lower()
+                if answer == "s":
+                    database.cell(row=acc, column = 1).value = ""
+                    database.cell(row=acc, column = 2).value = ""
+                    database.cell(row=acc, column = 3).value = ""
+                    databasexlsx.save(databasedir)
+                    print("success")
+                    return 0
+                else:
+                    print("Si, mejor no lo borramos.")
+                    return 0
+        print("No existe esa cuenta.")
+        return 0
+    else:
+        account = input("Que cuenta quieres borrar?: ")
+        #Recorremos la lista de cuentas
+        for acc in range(2, 1048576):
+            #Si la cuenta existe
+            if database.cell(row=acc, column = 1).value == account:
+                if database.cell(row=acc, column = 3).value == activeuser:
+                    if database.cell(row=acc, column = 2).value != 0:
+                        #Preguntamos si quiere confirmar el borrado
+                        answer = str(input(f"¿Estás seguro/a de eliminar la cuenta {account} que tiene {database.cell(row=acc, column = 2).value}€? s/n: ")).lower()
+                        if answer == "s":
+                            database.cell(row=acc, column = 1).value = ""
+                            database.cell(row=acc, column = 2).value = ""
+                            database.cell(row=acc, column = 3).value = ""
+                            databasexlsx.save(databasedir)
+                            print("success")
+                            return 0
+                        else:
+                            print("Si, mejor no lo borramos.")
+                            return 0
+                    else:
+                        database.cell(row=acc, column = 1).value = ""
+                        database.cell(row=acc, column = 2).value = ""
+                        database.cell(row=acc, column = 3).value = ""
+                        databasexlsx.save(databasedir)
+                        print("success")
+                        return 0
+                else:
+                    print("Esta cuenta no es tuya")
+                    return 0
+        print("No existe esa cuenta.")
+        return 0
+
 #Ciclo de programa
 while True:
     #Si no hay un usuario activo, se muestra el menú de login/register
@@ -332,9 +529,10 @@ while True:
         print("\n====================================================")
         print("//                   ARTIBANK SL                  //")
         print(f"//                    {activeuser}                   //")
-        print("//  1.Chequear cuentas        4.Depositar         //")
-        print("//  2.Crear cuentas           5.Retirar           //")
-        print("//  3.Saldo total             6.Transferencia     //")
+        print("//  1.Chequear cuentas        5.Depositar         //")
+        print("//  2.Crear cuentas           6.Retirar           //")
+        print("//  3.Saldo total             7.Transferencia     //")
+        print("//  4.Borrar cuenta           8.Deshabilitado     //")
         print("====================================================")
         print("//  98.Logout                 99.EXIT             //")
         print("====================================================")
@@ -345,14 +543,18 @@ while True:
             checkaccounts()
         elif int(opt)==2:
             createaccount()
-        #elif int(opt)==3:
-            #total()
+        elif int(opt)==3:
+            total()
         elif int(opt)==4:
-            deposit()
+            delaccount()
         elif int(opt)==5:
+            deposit()
+        elif int(opt)==6:
             withdrawal()
-        #elif int(opt)==6:
-            #transfer()
+        elif int(opt)==7:
+            transfer()
+        elif int(opt)==8:
+            print("PERO NO VES QUE ESTÁ DESHABILITADO")
         elif int(opt)==98:
             logout()
         elif int(opt)==99:
